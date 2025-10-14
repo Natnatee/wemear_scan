b10a90644162858ebea73c177ed2c662bd9cdad1
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-10-14 21:00:50 +0700

    pnpm i util

--- src/test.js
+++ src/test.js
@@ -1,5 +1,8 @@
 import { convertToLegacyFormat } from "./utils/convertData.js";
 import { sampleProjects } from "./make_data/sample_project2.js";
+import util from "util";
+
+//node src/test.js
 
 const legacyData = convertToLegacyFormat(sampleProjects[0]);
-console.log(legacyData);
+console.log(util.inspect(legacyData, { depth: null, colors: true }));
