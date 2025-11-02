0162472916476d1a0fdead5e0f4dcbcf13dcd3c9
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-11-02 16:53:46 +0700

    move file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
+    // Optional: helps with SPA routing if using client-side router
     historyApiFallback: true,
   },
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
+        main: resolve(__dirname, "index.html"),
+        face: resolve(__dirname, "face.html"),
       },
     },
   },
